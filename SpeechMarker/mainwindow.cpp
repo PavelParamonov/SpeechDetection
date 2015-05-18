a943c6a541ad9ca922a9864b561e1a21fa42653d
Pavel Paramonov <pa.pawka@gmail.com>
2015-05-18 20:10:29 +0300

    Added buttons "Load wav" and "Save markers" + .gitignore

--- SpeechMarker/mainwindow.cpp
+++ SpeechMarker/mainwindow.cpp
@@ -6,9 +6,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    pBtnLoadWav = new QPushButton("Load Wav File", this);
+    pBtnSaveMarkers = new QPushButton("Save markers", this);
 }
 
 MainWindow::~MainWindow()
 {
+    delete pBtnLoadWav;
+    delete pBtnSaveMarkers;
     delete ui;
 }
--- SpeechMarker/mainwindow.h
+++ SpeechMarker/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QPushButton *pBtnLoadWav, *pBtnSaveMarkers;
 };
 
 #endif // MAINWINDOW_H
